a8d04b6984be4cb430b36199cd74c65da74f4367
blackjacksike <46088869+blackjacksike@users.noreply.github.com>
2019-10-16 11:58:16 -0400

    Update 1
    Moved the while loop before the if statement and removed the else statement.

--- passwordProgram.cpp
+++ passwordProgram.cpp
@@ -11,26 +11,21 @@ int main(){
 	cin>>password;
 	cout<<"Password created. Now, try to log in. You have "<<numberofTriesLeft<<" tries left."<<endl;
 	cin>>passwordAttempt; //first try
+	while(passwordAttempt!=password && numberofTriesLeft>0){
+		--numberofTriesLeft;
+		if(numberofTriesLeft==0){
+			break;
+		}
+		cout<<"Acess denied. Please try again."<<endl;
+		cout<<"You have "<<numberofTriesLeft<<" tries or try left."<<endl;
+		cin>>passwordAttempt;
+	}
 	if(passwordAttempt==password){
 		cout<<"Access granted."<<endl;
 		std::getchar();
 	}
-	else{
-		--numberofTriesLeft; //there are now 2 tries left
-		while(passwordAttempt!=password && numberofTriesLeft>0){
-			cout<<"Acess denied. Please try again."<<endl;
-			cout<<"You have "<<numberofTriesLeft<<" tries or try left."<<endl;
-			cin>>passwordAttempt;
-			if(passwordAttempt!=password){
-				--numberofTriesLeft; /*if the second guess is wrong, it will leave the user with one last guess.*/
-			}
-		}
-		if(passwordAttempt==password){
-			cout<<"Access granted."<<endl;
-		}
-		if(numberofTriesLeft==0){
-			cout<<"Ah ah ah! You didn't say the magic word!"<<endl;
-		}
+	if(numberofTriesLeft==0){
+		cout<<"Ah ah ah! You didn't say the magic word!"<<endl;
 		std::getchar();
 	}
 	cout<<"Press Enter to exit."<<endl;
